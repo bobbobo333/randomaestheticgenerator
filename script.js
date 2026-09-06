44c37c0c24a2af814dfd2ea7b9b91f9712210a4a
bobbobo333 <bobbobo12346@gmail.com>
2026-09-06 12:10:30 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("generateButton");
+
+button.addEventListener("click", function () {
+    document.getElementById("hat").textContent = "BOHEMIAN → FLOPPY HAT";
+});
